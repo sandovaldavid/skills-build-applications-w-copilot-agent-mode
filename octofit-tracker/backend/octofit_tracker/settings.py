1caf69de0d13745c4fee9b662ce8fe627354b9ef
Juan David Sandoval Salvador <102970701+sandovaldavid@users.noreply.github.com>
2025-04-11 03:51:34 +0000

    Update ALLOWED_HOSTS in settings.py to include GitHub Codespaces URL

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-sp7kqxc91zrawp%%d*xbj@01cz4v0pikb(g8lz#j_*q*i3npty
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'urban-disco-r55rg4gw6vj2xvw5-8000.app.github.dev']
 
 
 # Application definition
